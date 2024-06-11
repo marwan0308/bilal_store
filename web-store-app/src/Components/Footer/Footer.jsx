ec7b34e016d4e32953f64256edfd8440d780a51d
logmar98 <marwanarbib15@gmail.com>
2024-06-11 12:45:47 +0100

    i just add footer

--- web-store-app/src/Components/Footer/Footer.jsx
+++ web-store-app/src/Components/Footer/Footer.jsx
@@ -3,8 +3,8 @@ import Style from './Footer.module.css';
 function Footer() {
 
     return (
-        <div className={Style.Footer}>
-        <h1>Footer</h1>
+        <div className={Style.footer}>
+            <h4>@2024 build by <a href="https://marwanarbib.wordpress.com/">marwan rbib</a></h4>
         </div>
     )
 }
